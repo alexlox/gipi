ff0feaa4c52f25998b4b31049df73f8da179d1f6
Alexandru Necula <alex.lox98@gmail.com>
2020-03-27 21:57:37 +0200

    working on login

--- gipi_app/views.py
+++ gipi_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+from gipi_app.models import User
 
 # Create your views here.
 
@@ -12,3 +13,18 @@ def index(request):
         }
     }
     return render(request, 'gipi_app/index.html', context)
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'gipi_app/login.html')
+
+    username = request.body['username'].strip()
+    password = request.body['password'].strip()
+
+    # if username == '' or password == '':
+    #     return HttpResponseBadRequest('{ "message": "Please fill all fields." }')
+    # if len(User.objects.filter(username=username)) != 0:
+    #     return HttpResponseBadRequest('{ "message": "Username already taken." }')
+
+    
